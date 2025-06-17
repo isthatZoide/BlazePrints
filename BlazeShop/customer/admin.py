b284cf00c79cf532d8444ed385b889d85577024b
Lucas Silva <Lucas.Silva@Inpact.pt>
2025-06-17 16:30:59 +0100

    C:\Users\Lucas.Silva\Desktop\Lucas\Shop

--- BlazeShop/customer/admin.py
+++ BlazeShop/customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CatalogueItem, Category, OrderModel
 
-# Register your models here.
+
+admin.site.register(CatalogueItem)
+admin.site.register(Category)
+admin.site.register(OrderModel)
